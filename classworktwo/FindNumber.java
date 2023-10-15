acf25d029e45172a982d3c44f4e4c937ef1a5567
gsaarthi <147967303+gsaarthi@users.noreply.github.com>
2023-10-15 12:21:47 +0530

    Add files via upload

---
+++ classworktwo/FindNumber.java
@@ -0,0 +1,23 @@
+package classworktwo;
+
+public class FindNumber {
+
+	
+		public static void main(String[] args) {
+			
+			int num_1 = 45;
+			
+			if (num_1>0) {
+				System.out.println("The number is a positive number");
+			}
+			else if(num_1<0) {
+				System.out.println("The number is a negative number");
+			}
+			else {
+				System.out.println("The number is a  neutral number");
+			}
+			
+		}
+
+	}
+
